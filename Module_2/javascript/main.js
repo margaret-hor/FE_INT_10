aa48b2b07ec1a7b68efa87e5201697ee8222726e
Marharyta Hordiiuk <hordiiukmargaret@gmail.com>
2025-12-11 17:44:17 +0100

    😭

--- Module_2/javascript/main.js
+++ Module_2/javascript/main.js
@@ -20,3 +20,14 @@ document.querySelector('#scroll-btn').addEventListener('click', function() {
     behavior: 'smooth'
   });
 });
+
+// toggle hero menu
+function toggleModal() {
+  const navbar = document.getElementById("top_nav");
+  const hamburgerWrapper = document.getElementById("hamburger_wrapper");
+  const hamburger = document.getElementById("hamburger");
+
+  navbar && navbar.classList.toggle("nav_active");
+  hamburgerWrapper && hamburgerWrapper.classList.toggle("hamburger_wrapper_active");
+  hamburger && hamburger.classList.toggle("hamburger_active");
+}
